8b6b8272e2030b1566f1b4321642b98b9530b98d
Admin <Admin@Admin-PC>
2018-03-17 16:10:05 +0600

    Init

---
+++ src/PuppyName.java
@@ -0,0 +1,17 @@
+
+public class PuppyName {
+	
+	public PuppyName(String name)  {
+		
+		System.out.println("Passed name is = " + name);
+			
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PuppyName myPuppy = new PuppyName ("Gafril.");
+		
+	}
+
+}
